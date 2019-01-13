7ddeeeb0f4001c52b00b60e403a625d606a10617
Germain Bossu <germainbossu@yahoo.fr>
2019-01-13 20:22:35 +0100

    SeedCheck.py: add more description about checks to be implemented

--- SeedCheck.py
+++ SeedCheck.py
@@ -21,19 +21,49 @@
 # * leverage on Debug.ini ? => is it really necessary… keep in mind "KISS"
 # * maybe need some config file ? => try to find a way without that
 # * TheCore Left-Right mirror and related headhaches with default keys
+# * argparse usage for debug => work first on core functions
+#	~ batch usage to select only given seeds by path, activate fancy verbose helper, skip quality checks
 #
-# Checks to be implemented:
-# * under construction
+###########################################$
+# Checks to be implemented
+###########################################$
+#
+# .SC2Hotkey checks:
+# * CommandConflictWithHotkeys
+#		direct access, or Shift+key commands
+#		=> need Defaults.ini, then rely on .SC2Hotkey
+# * SameCheck
+#		check that commands are consistent
+#		=> SameChecks.py?
+# * UnboundCommand
+# 		unbound command out of identified conflicts
+# 		unbound command in identified conflicts
+# * ConflictCheck
+#		for all commands check conflicts
+#		check that all commands are bound
+#		=> need Defaults.ini and ConflictChecks.py
+# * CommandRootConsistency
+#		commands are either <command> or <command>/<unit>
+#		we want to check that <command> are consistent with all <command>/<unit> as a general rule
+#		there are exception => to be stored in a config file
+#		=> need only .SC2Hotkey
 #
 # Quality checks: => low priority, maybe consider drop this feature
 # * EmptyDefault: check for empty Defaults.ini entries
 #	~ need to update Defaults.ini when a new command is not references
-# * MissingCommandCard:
+# * MissingConflict:
 #	~ report when a command has no conflict in Conflicts.py
+#
+# Questionable checks:
+# * Regression check
+#		Difficult to evaluate which commands are important out of game data conflict extraction
+# * TheCoreLeftRightMirror
+#		Maybe overlap with some workflow
 
 import configparser
 from enum import Enum
 
+## Global settings
 source_dir = 'hotkey_sources/'
 
 class ConfigParser(configparser.ConfigParser):
@@ -56,3 +86,19 @@ def load_ini_file(inputfilename):
 	inifile.allow_no_value=True
 	inifile.read(inputfilename)
 	return inifile
+
+################################################################################
+## one function per check
+## standard inputs: hotkey_file + output_file (<= file to drop all check report)
+
+################################################################################
+## general seed check function
+
+###############
+###  MAIN   ###
+###############
+
+## Load config files
+
+## Loop over all target seeds and produce report
+
